e3729e6adb1925cf8e49a75d0e0a0ffd0ae5eff8
nsamelson <salocin.sam@gmail.com>
2024-05-10 14:40:11 +1200

    generate xml dataset

--- tex2xml.py
+++ tex2xml.py
@@ -4,6 +4,8 @@ import xml.etree.ElementTree as ET
 from PIL import Image
 import networkx as nx
 import matplotlib.pyplot as plt
+from latex2mathml.exceptions import InvalidAlignmentError, DoubleSuperscriptsError, DenominatorNotFoundError
+from tqdm import tqdm
 
 
 # Dataset of formulas : https://huggingface.co/datasets/OleehyO/latex-formulas
@@ -13,18 +15,50 @@ import matplotlib.pyplot as plt
 # it with the im2latex-100K dataset, we obtained the cleaned_formulas dataset, which has 550K formula-image pairs."
 
 
-
 # data = load_dataset("OleehyO/latex-formulas", "raw_formulas",trust_remote_code=True) 
+# data = load_dataset("OleehyO/latex-formulas", "cleaned_formulas",trust_remote_code=True) 
+conversion_stats = {
+    "success":0,
+    "align":0,
+    "superscript":0,
+    "denominator":0,
+    "dunno":0,
+}
+
 data = load_dataset("OleehyO/latex-formulas", "cleaned_formulas",trust_remote_code=True) 
 
-example = data["train"][101]
-# print(example["latex_formula"])
-Image.Image.save(example["image"],"out/example.jpeg")
+# Initialize a string to store all XML documents
+root = ET.Element("formulas")
 
-mathml_output = converter.convert(example["latex_formula"])
-test = converter.convert_to_element(example["latex_formula"])
+# Iterate over all equations in the dataset
+for i, formula in enumerate(tqdm(data["train"])):
+    latex_formula = formula["latex_formula"]
 
-tree = ET.ElementTree(test)
-tree.write("out/test.xml", encoding="utf-8", xml_declaration=True)
+    # Convert LaTeX formula to MathML
+    try:
+        mathml_element = converter.convert_to_element(latex_formula)
+        conversion_stats["success"] += 1
+    except InvalidAlignmentError:        
+        try:
+            cleaned = latex_formula.replace(r"\begin{align*}","").replace(r"\end{align*}","")
+            mathml_element = converter.convert_to_element(cleaned)
+        except Exception as e:
+            # print(f"Error removing alginment at index {i}: {latex_formula} -- {e}")
+            conversion_stats["align"] += 1
+    except DoubleSuperscriptsError:
+        # print(f"Error removing DoubleSuperscript at index {i}")
+        conversion_stats["superscript"] += 1
 
+    except DenominatorNotFoundError:
+        # print(f"Error converting formula at index {i}")
+        conversion_stats["denominator"] += 1
+    
+    except Exception as e:
+        # print(f"Exception {e}")
+        conversion_stats["dunno"] += 1
 
+    root.append(mathml_element)
+print(f"The latex formulas were converted, here are the stats : {conversion_stats}")
+
+tree = ET.ElementTree(root)
+tree.write("dataset/equations.xml", encoding="utf-8", xml_declaration=True)
