664d682417852a06f0770924f7c1f1dba5d64dc0
nsamelson <salocin.sam@gmail.com>
2024-05-28 20:47:11 +1200

    train model functional

--- models/Graph/GraphAutoEncoder.py
+++ models/Graph/GraphAutoEncoder.py
@@ -2,17 +2,17 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv,GraphConv
 from torch_geometric.utils import from_networkx
 from torch_geometric.data import DataLoader, Data
+from preprocessing.GraphEmbedder import GraphEmbedder
 
-# from preprocessing.GraphEmbedder import MATHML_TAGS,
 
 class GraphEncoder(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super(GraphEncoder, self).__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, out_channels)
+        self.conv1 = GraphConv(in_channels, hidden_channels)
+        self.conv2 = GraphConv(hidden_channels, out_channels)
 
     def forward(self, x, edge_index):
         x = F.relu(self.conv1(x, edge_index))
@@ -22,8 +22,8 @@ class GraphEncoder(torch.nn.Module):
 class GraphDecoder(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super(GraphDecoder, self).__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, out_channels)
+        self.conv1 = GraphConv(in_channels, hidden_channels)
+        self.conv2 = GraphConv(hidden_channels, out_channels)
 
     def forward(self, x, edge_index):
         x = F.relu(self.conv1(x, edge_index))
@@ -31,34 +31,23 @@ class GraphDecoder(torch.nn.Module):
         return x
 
 class GraphAutoencoder(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, latent_channels):
+    def __init__(self, in_channels, hidden_channels, latent_channels,embedder=GraphEmbedder,mathml_tags=[]):
         super(GraphAutoencoder, self).__init__()
+        self.embedder = embedder
+        self.mathml_tags = mathml_tags
+
         self.encoder = GraphEncoder(in_channels, hidden_channels, latent_channels)
         self.decoder = GraphDecoder(latent_channels, hidden_channels, in_channels)
 
-    def forward(self, x, edge_index):
-        z = self.encoder(x, edge_index)
-        x_hat = self.decoder(z, edge_index)
+    def forward(self,batch):
+        # EMBED INTO a VECTOR of size 31 + 4 * (embed_dim - 1) = 67
+        # embedded_batch = self.embedder.embed_into_vector(batch.x)
+        # print(embedded_batch.shape, batch.edge_index.shape)
+ 
+        # Perform encoding and decoding
+        z = self.encoder(batch.x, batch.edge_index)
+        x_hat = self.decoder(z, batch.edge_index)
         return x_hat, z
     
 
 
-class YourModel(nn.Module):
-    def __init__(self, embedder,mathml_tags):
-        super(YourModel, self).__init__()
-        self.embedder = embedder
-        self.mathml_tags = mathml_tags
-        # Define other layers of your model here
-
-    def forward(self, batch):
-        batch_indices = batch.x
-        embedded_batch = []
-        for tag in ["mi", "mo", "mtext"]:
-            tag_indices = batch_indices[:, self.mathml_tags.index(tag)]
-            embedded_tag = self.embedder.embeddings[tag](tag_indices)
-            embedded_batch.append(embedded_tag)
-        # Concatenate embeddings for all tags and other features
-        embedded_batch = torch.cat(embedded_batch, dim=1)
-        # Pass through other layers of your model
-        # output = self.some_layers(embedded_batch)
-        # return output
--- models/train.py
+++ models/train.py
@@ -1,20 +1,96 @@
 import json
+from matplotlib import pyplot as plt
 import numpy as np
+# from sklearn.base import accuracy_score
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.manifold import TSNE
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.data import Data, InMemoryDataset, DataLoader
+from torch_geometric.data import Data, InMemoryDataset, collate, Batch
+from torch_geometric.loader import DataLoader
 from torch_geometric.nn import GCNConv
 import xml.etree.ElementTree as ET
-from preprocessing.GraphEmbedder import GraphEmbedder
+
+from tqdm import tqdm
+from preprocessing.GraphEmbedder import GraphEmbedder, MATHML_TAGS
+from models.Graph.GraphDataset import GraphDataset
+from models.Graph.GraphAutoEncoder import GraphAutoencoder
+import random
+
 
 
 def main():
+    # Fix the randomness
+    seed_value = 0
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)  # if using multiple GPUs
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
     print("Loading dataset...")
-    dataset = torch.load("dataset/graph_formulas_katex_debug.pt")
-    print("Finished loading dataset!")
-    print(dataset)
-    # TODO: build dataset with GraphDataset Class
-    # TODO: import model
-    # TODO: create train loop (separate function)
-    # TODO: poof we have a trained model
+    dataset_path = "dataset/graph_formulas_katex_debug.pt"
+    dataset = torch.load(dataset_path)
+
+    embedder = GraphEmbedder(scale_by_freq=False)
+
+    # def collate_fn(data_list):
+    #     batch = Batch.from_data_list(data_list)
+    #     batch.x = embedder.embed_into_vector(batch.x)
+    #     return batch
+
+    # Create a DataLoader
+    batch_size = 3
+    # dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True,num_workers=0)
+    dataloader = EmbeddedDataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0, embedder=embedder)
+
+
+
+    in_channels = len(MATHML_TAGS) + 4 * (embedder.embedding_dims - 1) 
+
+    model = GraphAutoencoder(in_channels,16,8,embedder,MATHML_TAGS)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    criterion = torch.nn.MSELoss()
+
+    # Training loop
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        total_loss = 0
+        for batch in dataloader:
+            optimizer.zero_grad()
+            x_hat, z = model(batch)
+            loss = criterion(x_hat, batch.x)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        
+        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(dataloader)}")
+
+    # Example of using the model for inference
+    model.eval()
+    with torch.no_grad():
+        for batch in dataloader:
+            x_hat, z = model(batch)
+            print("Original Node Features:", batch.x)
+            print("Reconstructed Node Features:", x_hat)
+            # print("what is z: ",z)
+            # Calculate reconstruction error (e.g., Mean Squared Error)
+            reconstruction_error = np.mean((batch.x.numpy() - x_hat.detach().numpy()) ** 2)
+            print("Reconstruction Error:", reconstruction_error)
+
+
+    
+class EmbeddedDataLoader(DataLoader):
+    def __init__(self, *args, embedder=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.embedder = embedder
+
+    def __iter__(self):
+        for batch in super().__iter__():
+            # Apply the embedding function to the batch
+            batch.x = self.embedder.embed_into_vector(batch.x)
+            yield batch
