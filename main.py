99c80e56af501093f7b77d9e9f5ecefa476dfdce
nsamelson <salocin.sam@gmail.com>
2024-06-05 18:16:57 +1200

    add new requirements for hyperparam search

--- main.py
+++ main.py
@@ -2,16 +2,17 @@
 import argparse
 from preprocessing import xml2graph, test, tex2xml
 import utils.stats as stats
-from models import train
+from models import train, search
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-tx", "--tex2xml", metavar='\b', default=False, type=bool, help="Default False. Download Tex equations, convert to XML and save dataset")
     parser.add_argument("-xg", "--xml2graph", metavar='\b', default=False, type=bool, help="Default False. Load XML, convert to graph and save dataset")
-    parser.add_argument("-s", "--stats", metavar='\b', default=False, type=bool, help="Default False. Create stats")
+    parser.add_argument("-st", "--stats", metavar='\b', default=False, type=bool, help="Default False. Create stats")
     parser.add_argument("-te", "--test", metavar='\b', default=False, type=bool, help="Default False. Test things")
     parser.add_argument("-tr", "--train", metavar='\b', default=False, type=bool, help="Default False. Train things")
+    parser.add_argument("-se", "--search", metavar='\b', default=False, type=bool, help="Default False. Search hyperparams")
 
 
 
@@ -29,6 +30,9 @@ if __name__=="__main__":
 
     if args.train:
         train.main()
+    
+    if args.search:
+        search.main()
 
     if args.test:
         test.main()
